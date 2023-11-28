49d6f892030e02f8b9e4443d4f4eb31df6c8bad2
Ziemowit Szymaniak <zkskik@gmail.com>
2023-11-28 16:59:05 +0000

    init backend

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,18 @@ const httpServer = express();
 const dialer = require('dialer').Dialer;
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
 const config = {
  url: 'https://uni-call.fcc-online.pl',
  login: 'focus11',
  password: '#b0yo2iwedsk'
 };
+httpServer.use(bodyParser.json());
+httpServer.use(cors());
+httpServer.use((req, res, next) => {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+ next();
+});
 dialer.configure(config); 
 // Serwer nasłuchuje na porcie 3000
 httpServer.listen(3000, function () {
@@ -20,32 +26,23 @@ httpServer.get('/call/:number1/:number2', (req, res) => {
  dialer.call(number1,number2);
  res.json({success: true});
 })
-
-httpServer.use(bodyParser.json());
-httpServer.use(cors());
-httpServer.use((req, res, next) => {
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
- next();
-});
-
 httpServer.post('/call/', async (req, res) => {
-    const number1 = req.body.number;
-    const number2 = '533833428' // tutaj dejemy swój numer
-    console.log('Dzwonie', number1, number2)
-    const bridge = await dialer.call(number1, number2);
-    let interval = setInterval(async () => {
-      let status = await bridge.getStatus();
-      console.log(status)
-      if (
-        status === "ANSWERED" ||
-        status === "FAILED" ||
-        status === "BUSY" ||
-        status === "NO ANSWER"
-      ) {
-        console.log("stop");
-        clearInterval(interval);
-      }
-    }, 2000);
-    res.json({ success: true });
-   })
+  const number1 = req.body.number;
+  const number2 = '533833428' // tutaj dejemy swój numer
+  console.log('Dzwonie', number1, number2)
+  const bridge = await dialer.call(number1, number2);
+  let interval = setInterval(async () => {
+    let status = await bridge.getStatus();
+    console.log(status)
+    if (
+      status === "ANSWERED" ||
+      status === "FAILED" ||
+      status === "BUSY" ||
+      status === "NO ANSWER"
+    ) {
+      console.log("stop");
+      clearInterval(interval);
+    }
+  }, 2000);
+  res.json({ success: true });
+ })
